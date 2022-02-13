cec09d2f9a9c085bc53ad56271076d5ff1ea2d0c
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2022-02-13 23:32:22 +0900

    systemd

---
+++ src/make_systemd_service.rb
@@ -0,0 +1,26 @@
+require_relative "conf"
+
+def make_systemd_service(appname)
+  script = Dir.pwd + '/main.rb'
+
+  <<~EOS
+  [Unit]
+  Description = #{appname} daemon
+
+  [Service]
+  ExecStart = /usr/bin/ruby #{script}
+  Restart = always
+  Type = simple
+  User = #{ENV['USER']}
+
+  [Install]
+  WantedBy = multi-user.target
+  EOS
+end
+
+if __FILE__ == $0
+  appname = ARGV.shift || exit
+  fname = "app-#{appname}.service"
+  File.write(fname, make_systemd_service(appname))
+  system("sudo cp #{fname} /etc/systemd/system/")
+end
